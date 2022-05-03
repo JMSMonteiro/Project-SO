ce7ea42aa5ecee614bdf61597b33042bce973519
Jmmonteiro <monteirojmig@gmail.com>
2022-05-03 15:32:53 +0100

    Edge Server basic skeleton of how will work

--- src/edge_server.c
+++ src/edge_server.c
@@ -15,17 +15,27 @@
 
 #define VCPU_NUMBER 2
 
+typedef struct {
+    int processing_power;
+    int is_high_performance;
+} thread_settings;
+
 pthread_t v_cpu[VCPU_NUMBER];
 pthread_mutex_t edge_server_thread_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t terminate_server_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t end_of_maintenance_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t high_performance_mode_cond = PTHREAD_COND_INITIALIZER;
 
 static int server_is_running = 1;
+static int server_needs_maintenance = 0;
+static int performance_mode = 1;
 static int server_index = -1;
 
 void start_edge_server(edge_server *server_config, int server_shm_position, int server_number, int* unnamed_pipe) {
     maintenance_message message_rcvd;
+    thread_settings t_settings[VCPU_NUMBER];
     char log_message[LOG_MESSAGE_SIZE];
-    int thread_id[VCPU_NUMBER] = { server_config->v_cpu1, server_config->v_cpu2};
+    int vcpu_power[VCPU_NUMBER] = { server_config->v_cpu1, server_config->v_cpu2};
     int i;
 
     signal(SIGINT, SIG_IGN);
@@ -37,6 +47,7 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
 
     sem_wait(mutex_servers);
     server_config->server_pid = getpid();
+    performance_mode = server_config->performance_mode;
     sem_post(mutex_servers);
 
     sem_wait(mutex_servers);
@@ -45,6 +56,9 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
                                 server_config->name);
     sem_post(mutex_servers);
 
+    // ? Print => "Info: Edge server {Server_name} Created"
+    handle_log(log_message); 
+    
     // ? Handle Pipe
     // ? The end of the pipe in the server is meant to be read from
     // ? Task mngr => Edge server (Tasks) 
@@ -57,9 +71,6 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
         close(unnamed_pipe[(i * 2)]);       // 0 = Read
         close(unnamed_pipe[(i * 2) + 1]);   // 1 = Write
     }
-    
-    // ? Print => "Info: Edge server {Server_name} Created"
-    handle_log(log_message); 
 
     #ifdef DEBUG
     sem_wait(mutex_servers);
@@ -68,19 +79,28 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
     #endif
 
     // Create threads
-    for (i = 0; i < VCPU_NUMBER; i++) {
-        #ifdef DEBUG
-        sem_wait(mutex_servers);
-        printf("\tServer %s Making thread #%d\n", server_config->name, i + 1);
-        sem_post(mutex_servers);
-        #endif
+    #ifdef DEBUG
+    sem_wait(mutex_servers);
+    printf("\tServer %s Making thread #%d\n", server_config->name, 1);
+    sem_post(mutex_servers);
+    #endif
+    t_settings[0].processing_power = vcpu_power[0];
+    t_settings[0].is_high_performance = vcpu_power[0] > vcpu_power[1] ? 1 : 0;
+    pthread_create(&v_cpu[0], NULL, edge_thread, &t_settings[0]);
     
-        pthread_create(&v_cpu[i], NULL, edge_thread, &thread_id[i]);
-    }
+    #ifdef DEBUG
+    sem_wait(mutex_servers);
+    printf("\tServer %s Making thread #%d\n", server_config->name, 2);
+    sem_post(mutex_servers);
+    #endif
+    t_settings[1].processing_power = vcpu_power[1];
+    t_settings[1].is_high_performance = vcpu_power[1] > vcpu_power[0] ? 1 : 0;
+    pthread_create(&v_cpu[1], NULL, edge_thread, &t_settings[1]);
 
     while (1) {
         msgrcv(message_queue_id, &message_rcvd, sizeof(maintenance_message) - sizeof(long), (server_index + 1), 0);
         if (message_rcvd.stop_flag) {
+            server_needs_maintenance = 1;
             snprintf(log_message, LOG_MESSAGE_SIZE, 
                                 "INFO: Server %d Stopped, going to maintenance for (%ds)",
                                 server_index,
@@ -91,10 +111,15 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
             #endif
         }
         else {
+            server_needs_maintenance = 0;
             snprintf(log_message, LOG_MESSAGE_SIZE, 
                                 "INFO: Server %d Maintained, going back to work",
                                 server_index);
             handle_log(log_message);
+            
+            pthread_mutex_lock(&edge_server_thread_mutex);
+            pthread_cond_broadcast(&end_of_maintenance_cond);
+            pthread_mutex_unlock(&edge_server_thread_mutex);
             #ifdef DEBUG
             printf("\t[SERVER %d] Received message to RESUME\n", server_index);
             #endif
@@ -105,13 +130,16 @@ void start_edge_server(edge_server *server_config, int server_shm_position, int 
 void handle_edge_shutdown(int signum) {
     int i;
     #ifdef DEBUG
-    printf("Shutting down Edge Server <=> %d\n", getpid());
+    printf("\t\t\tShutting down Edge Server <=> %d\n", getpid());
     #endif
 
     server_is_running = 0;
 
+    // Signal everything to make sure threads  terminate and don't get stuck
     pthread_mutex_lock(&edge_server_thread_mutex);
     pthread_cond_broadcast(&terminate_server_cond);
+    pthread_cond_broadcast(&high_performance_mode_cond);
+    pthread_cond_broadcast(&end_of_maintenance_cond);
     pthread_mutex_unlock(&edge_server_thread_mutex);
 
     for (i = 0; i < VCPU_NUMBER; i++) {
@@ -128,18 +156,56 @@ void handle_edge_shutdown(int signum) {
 }
 
 void *edge_thread (void* p) {
+    thread_settings settings = *((thread_settings *) p);
+    struct timespec remaining, request = {1, 0};
     #ifdef DEBUG
-    int w_id = *((int *) p);
-    printf("I'm a thread | My power is %d!\n", w_id);
+    printf("I'm a vCPU thread | [POWER] = %d! [Performance] = %d\n", settings.processing_power, settings.is_high_performance);
     #endif
 
-    pthread_mutex_lock(&edge_server_thread_mutex);
-    while (server_is_running) {
-        pthread_cond_wait(&terminate_server_cond, &edge_server_thread_mutex);
+    #ifdef DEBUG
+    if (settings.is_high_performance) {
+        printf("\t\tI\'m the high performance thread!\n");
     }
-    pthread_mutex_unlock(&edge_server_thread_mutex);
+    #endif
+
+    while (server_is_running) {
+        if (server_needs_maintenance) {
+            #ifdef DEBUG
+            printf("\t[THREAD] with [POWER] = %d [STOPPING]!\n", settings.processing_power);
+            #endif
+            pthread_mutex_lock(&edge_server_thread_mutex);
+            pthread_cond_wait(&end_of_maintenance_cond, &edge_server_thread_mutex);
+            pthread_mutex_unlock(&edge_server_thread_mutex);
+            #ifdef DEBUG
+            printf("\t\t[THREAD] with [POWER] = %d [RESUMING]!\n", settings.processing_power);
+            #endif
+        }
+        else if (settings.is_high_performance == 1 && performance_mode != 2) {
+            pthread_mutex_lock(&edge_server_thread_mutex);
+            pthread_cond_wait(&high_performance_mode_cond, &edge_server_thread_mutex);
+            pthread_mutex_unlock(&edge_server_thread_mutex);
+        }
+        else {
+            #ifdef DEBUG
+            printf("\tThread with %d POWER working\n", settings.processing_power);
+            #endif
+            nanosleep(&request, &remaining);
+        }
+    }
+
+
+    // pthread_mutex_lock(&edge_server_thread_mutex);
+    // while (server_is_running) {
+    //     pthread_cond_wait(&terminate_server_cond, &edge_server_thread_mutex);
+    // }
+    // pthread_mutex_unlock(&edge_server_thread_mutex);
 
     // TODO: Insert thread logic here
+
+
+    #ifdef DEBUG
+    printf("\tExiting from vcpu thread!\n");
+    #endif
 
     pthread_exit(NULL);
 }
